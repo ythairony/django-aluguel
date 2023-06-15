65c4d6632912ddcd2e411d7a63530a2fece47d00
Yuri Thairony <yurithairony@hotmail.com>
2023-06-15 08:19:25 -0300

    Corrigindo o arquivo DAO

--- aluguel/api/daos.py
+++ aluguel/api/daos.py
@@ -5,32 +5,27 @@ from .models import Item
 
 class ItemDAO:
     #Recupera a lista de itens cadastrados
-    def list_item(self, request):
+    def list_item(self):
         item_list = Item.objects.all()
-        context = {'item_list': item_list}
-        return render(request, 'item/listItem.html', context)
-
-    #Redirecionador para o formulário de cadastro de item
-    def form_item(self, request):
-        return render(request, 'item/formItem.html')
+        return item_list
 
     #Salva o novo item e volta para listagem de itens
-    def save_item(self, request):
-        i = Item(name=request.POST['name'], 
-                 description=request.POST['description'])
+    def save_item(self, nome, descricao):
+        i = Item(name=nome, description=descricao)
         i.save()
-        return redirect('/listItem')
+        
 
     #Deleta um item e volta para listagem de itens
     def delete_item(self, id):
         i = Item.objects.get(pk=id)
         i.delete()
-        return redirect('/listItem')
+        
     
     #Pega um item pelo ID e enviar para o form de edição
-    def detail_item(self, request, id):
+    def detail_item(self, id):
         item = Item.objects.get(pk=id)
-        return render(request, 'item/formEditItem.html', {'item': item} )
+        return item
+        
 
     #Atualiza um item e volta para listagem
     def update_item(self, request, id):
@@ -38,4 +33,4 @@ class ItemDAO:
         i.name = request.POST['name']
         i.description = request.POST['description']
         i.save()
-        return redirect('/listItem')        
+               
--- aluguel/api/views.py
+++ aluguel/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .daos import ItemDAO
 
@@ -12,19 +12,30 @@ class ItemViews:
         self.item_dao = ItemDAO()
 
     def list_item(self, request):
-        return self.item_dao.list_item(request)
+        item_list = ItemDAO.list_item(request)
+        context = {'item_list': item_list}
+        return render(request, 'item/listItem.html', context)
+    
 
     def form_item(self, request):
-        return self.item_dao.form_item(request)
+        # return self.item_dao.form_item(request)
+        return render(request, 'item/formItem.html')
 
     def save_item(self, request):
-        return self.item_dao.save_item(request)
+        name=request.POST['name']
+        description=request.POST['description']
+        self.item_dao.save_item(name, description)
+        return redirect('/listItem')
 
     def delete_item(self, request, id):
-        return self.item_dao.delete_item(id)
+        self.item_dao.delete_item(id)
+        return redirect('/listItem')
 
     def detail_item(self, request, id):
-        return self.item_dao.detail_item(request, id)
+        item = ItemDAO.detail_item(request, id)
+        context = {'item': item}
+        return render(request, 'item/formEditItem.html', context)
         
     def update_item(self, request, id):
-        return self.item_dao.update_item(request, id)
+        self.item_dao.update_item(request, id)
+        return redirect('/listItem') 
